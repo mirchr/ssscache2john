72201c8e54f1d851f8f8c3f54ef1d4c72abf8b28
Rich Mirch <mirchr@users.noreply.github.com>
2019-07-13 16:35:11 -0500

    Initial version

---
+++ ssscache2john.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# Author: Rich Mirch @0xm1rch
+# Description: Convert SSSD LDAP cache file to John The Ripper format
+# Tested on: Kali GNU/Linux Rolling 2019.1
+#
+# Example:
+#    ./ssscache2john.py /var/lib/sss/cache_LDAP.ldb
+#
+# Install dependencies
+# apt install sssd-common python-ldb
+
+from __future__ import print_function
+import ldb
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.stderr.write("Usage: %s <cache.ldb>\n" % sys.argv[0])
+        sys.exit(1)
+
+    conn = ldb.Ldb(sys.argv[1])
+    for record in conn.search("CN=SYSDB"):
+        if "CN=USERS" in str(record.dn).upper():
+            for item in record.items():
+                if item[0] == "name":
+                    username = str(item[1])
+                elif item[0] == "cachedPassword":
+                    cachedPassword = str(item[1])
+            print(username + ":" + cachedPassword)
